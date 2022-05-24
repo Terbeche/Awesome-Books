c78237399ca2aaec87927159a3f7ab34d700320d
ceemos <olaide.suleiman96@gmail.com>
2022-05-24 13:40:48 +0100

    Append books to the Dom

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-const bookForm = document.getElementById("bookForm");
-const booksContainer = document.querySelector(".books");
-const titleInput = bookForm["title"];
-const authorInput = bookForm["author"];
-const books = JSON.parse(localStorage.getItem("books")) || [];
+const bookForm = document.getElementById('bookForm');
+const booksContainer = document.querySelector('.books');
+const titleInput = bookForm.title;
+const authorInput = bookForm.author;
+const books = JSON.parse(localStorage.getItem('books')) || [];
 
 const addBook = (title, author) => {
-    books.push({
+  books.push({
     title,
     author,
   });
 
-  localStorage.setItem("books", JSON.stringify(books));
+  localStorage.setItem('books', JSON.stringify(books));
 
   return { title, author };
 };
@@ -18,31 +18,42 @@ const addBook = (title, author) => {
 const createBookElement = ({ title, author }) => {
   // Create elements
 
-  const bookDiv = document.createElement("div");
-  const bookTitle = document.createElement("h2");
-  const bookAuthor = document.createElement("h3");
-  const bookRemove =  document.createElement("button");
-  const bookHr =  document.createElement("hr");
+  const bookDiv = document.createElement('div');
+  const bookTitle = document.createElement('h2');
+  const bookAuthor = document.createElement('h3');
+  const bookRemove = document.createElement('button');
+  const bookHr = document.createElement('hr');
 
   // Fill the content
-  bookTitle.innerText = "Book title: " + title;
-  bookAuthor.innerText = "Book author: " + author;
-  bookRemove.innerText = "Remove";
+  bookTitle.innerText = `Book title: ${title}`;
+  bookAuthor.innerText = `Book author: ${author}`;
+  bookRemove.innerText = 'Remove';
 
-
-   bookRemove.onclick =  function() {
-        bookRemove.parentElement.remove();
-        for(let i = 0;i < books.length ;i += 1){
-            if(books[i]['title'] === title && books[i]['author'] === author){
-                 myIndex = i;
-            }
-        }
-       books.splice(myIndex,1);
-        localStorage.setItem("books", JSON.stringify(books));
+  bookRemove.onclick = function () {
+    bookRemove.parentElement.remove();
+    let myIndex;
+    for (let i = 0; i < books.length; i += 1) {
+      if (books[i].title === title && books[i].author === author) {
+        myIndex = i;
+      }
+    }
+    books.splice(myIndex, 1);
+    localStorage.setItem('books', JSON.stringify(books));
   };
 
   // Add to the DOM
-  bookDiv.append(bookTitle, bookAuthor,bookRemove,bookHr);
+  bookDiv.append(bookTitle, bookAuthor, bookRemove, bookHr);
   booksContainer.appendChild(bookDiv);
+};
+books.forEach(createBookElement);
 
+bookForm.onsubmit = (e) => {
+  e.preventDefault();
+
+  const newBook = addBook(titleInput.value, authorInput.value);
+
+  createBookElement(newBook);
+
+  titleInput.value = '';
+  authorInput.value = '';
 };
