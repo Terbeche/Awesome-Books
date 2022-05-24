950d51a701eb2df0705a78b8478aa5915e5ef0a3
Terbeche Mustapha <termus96@gmail.com>
2022-05-24 13:54:38 +0100

    add basic html,css and add & remove fucntion in javascript

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const bookForm = document.getElementById("bookForm");
+const booksContainer = document.querySelector(".books");
+const titleInput = bookForm["title"];
+const authorInput = bookForm["author"];
+const books = JSON.parse(localStorage.getItem("books")) || [];
+
+const addBook = (title, author) => {
+    books.push({
+    title,
+    author,
+  });
+
+  localStorage.setItem("books", JSON.stringify(books));
+
+  return { title, author };
+};
+
+const createBookElement = ({ title, author }) => {
+  // Create elements
+
+  const bookDiv = document.createElement("div");
+  const bookTitle = document.createElement("h2");
+  const bookAuthor = document.createElement("h3");
+  const bookRemove =  document.createElement("button");
+  const bookHr =  document.createElement("hr");
+
+  // Fill the content
+  bookTitle.innerText = "Book title: " + title;
+  bookAuthor.innerText = "Book author: " + author;
+  bookRemove.innerText = "Remove";
+
+
+   bookRemove.onclick =  function() {
+        bookRemove.parentElement.remove();
+        for(let i = 0;i < books.length ;i += 1){
+            if(books[i]['title'] === title && books[i]['author'] === author){
+                 myIndex = i;
+            }
+        }
+       books.splice(myIndex,1);
+        localStorage.setItem("books", JSON.stringify(books));
+  };
+
+  // Add to the DOM
+  bookDiv.append(bookTitle, bookAuthor,bookRemove,bookHr);
+  booksContainer.appendChild(bookDiv);
+
+};
