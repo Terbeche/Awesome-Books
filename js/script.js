d1f531049346a9b5d59feffdd4aef0515051aabc
Terbeche Mustapha <termus96@gmail.com>
2022-05-26 09:47:58 +0100

    add contact section and time and footer

--- js/script.js
+++ js/script.js
@@ -66,3 +66,21 @@ bookForm.onsubmit = (e) => {
   titleInput.value = '';
   authorInput.value = '';
 };
+
+// set time
+const displayDate = () => {
+  const date = new Date();
+  const options = {
+    weekday: undefined,
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+  const [month, time] = [
+    date.toLocaleDateString(undefined, options),
+    date.toLocaleTimeString().toLocaleLowerCase(),
+  ];
+  document.getElementById('time').innerHTML = `${month}, ${time}`;
+};
+displayDate();
+setInterval(displayDate, 1000);
