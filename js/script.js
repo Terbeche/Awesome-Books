56f0c2006f5ce64d07747e3459fdeb7b8d6977d8
ceemos <olaide.suleiman96@gmail.com>
2022-05-26 11:17:33 +0100

    Display sections through nav

--- js/script.js
+++ js/script.js
@@ -89,13 +89,26 @@ const link = document.querySelectorAll('.nav-link');
 const booksListSection = document.querySelector('#books-list');
 const addBookSection = document.querySelector('#input-fields');
 const contactSection = document.querySelector('.contact');
+const body = document.querySelector('body');
+const homebutton = document.querySelector('.navbar-brand');
 const sections = [booksListSection, addBookSection, contactSection];
+
+body.onload = function(){
+  link[0].classList.add('active');
+  // link[0].classList.remove('desactive');
+  link[1].classList.remove('active');
+  link[2].classList.remove('active');
+  sections[0].classList.remove('d-none');
+  sections[1].classList.add('d-none');
+  sections[2].classList.add('d-none');
+};
+
 
 link[0].addEventListener('click', () => {
   link[0].classList.add('active');
-  link[0].classList.remove('desactive');
-  link[1].classList.add('desactive');
-  link[2].classList.add('desactive');
+  // link[0].classList.remove('desactive');
+  link[1].classList.remove('active');
+  link[2].classList.remove('active');
   sections[0].classList.remove('d-none');
   sections[1].classList.add('d-none');
   sections[2].classList.add('d-none');
@@ -103,9 +116,9 @@ link[0].addEventListener('click', () => {
 
 link[1].addEventListener('click', () => {
   link[1].classList.add('active');
-  link[1].classList.remove('desactive');
-  link[0].classList.add('desactive');
-  link[2].classList.add('desactive');
+  // link[0].classList.remove('desactive');
+  link[1].classList.remove('active');
+  link[2].classList.remove('active');
   sections[1].classList.remove('d-none');
   sections[0].classList.add('d-none');
   sections[2].classList.add('d-none');
@@ -113,10 +126,16 @@ link[1].addEventListener('click', () => {
 
 link[2].addEventListener('click', () => {
   link[2].classList.add('active');
-  link[2].classList.remove('desactive');
-  link[0].classList.add('desactive');
-  link[1].classList.add('desactive');
+   // link[0].classList.remove('desactive');
+   link[1].classList.remove('active');
+   link[2].classList.remove('active');
   sections[2].classList.remove('d-none');
   sections[0].classList.add('d-none');
   sections[1].classList.add('d-none');
 });
+
+link.forEach(link =>{
+  if(!link.classList.contains("active")){
+
+  }
+})
