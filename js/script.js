22c35c68569f8eeb782f34c5a08fadf95ab003c7
Terbeche Mustapha <termus96@gmail.com>
2022-05-26 10:47:02 +0100

    add navigation links

--- js/script.js
+++ js/script.js
@@ -84,3 +84,39 @@ const displayDate = () => {
 };
 displayDate();
 setInterval(displayDate, 1000);
+
+const link = document.querySelectorAll('.nav-link');
+const booksListSection = document.querySelector('#books-list');
+const addBookSection = document.querySelector('#input-fields');
+const contactSection = document.querySelector('.contact');
+const sections = [booksListSection, addBookSection, contactSection];
+
+link[0].addEventListener('click', () => {
+  link[0].classList.add('active');
+  link[0].classList.remove('desactive');
+  link[1].classList.add('desactive');
+  link[2].classList.add('desactive');
+  sections[0].classList.remove('d-none');
+  sections[1].classList.add('d-none');
+  sections[2].classList.add('d-none');
+});
+
+link[1].addEventListener('click', () => {
+  link[1].classList.add('active');
+  link[1].classList.remove('desactive');
+  link[0].classList.add('desactive');
+  link[2].classList.add('desactive');
+  sections[1].classList.remove('d-none');
+  sections[0].classList.add('d-none');
+  sections[2].classList.add('d-none');
+});
+
+link[2].addEventListener('click', () => {
+  link[2].classList.add('active');
+  link[2].classList.remove('desactive');
+  link[0].classList.add('desactive');
+  link[1].classList.add('desactive');
+  sections[2].classList.remove('d-none');
+  sections[0].classList.add('d-none');
+  sections[1].classList.add('d-none');
+});
